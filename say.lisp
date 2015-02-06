546d74aff434b9ab25835f2cb8247ce61c4410b3
Collin Bell <slightlycyborg@gmail.com>
2015-02-05 21:10:15 -0500

    dumb response

--- say.lisp
+++ say.lisp
@@ -3,3 +3,7 @@
 (defun say (words)
   (script
    (concatenate 'string "echo " words "| festival --tts")))
+
+(defun respond_dumb(words)
+  (say
+   (concatenate 'string "I can not reason right now so I will just respond by copying you " words)))
