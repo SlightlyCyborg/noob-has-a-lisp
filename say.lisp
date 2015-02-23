0f94fd6c17e14bd524e6210a3f053be97bc39eb9
slightlycyborg <taggartmachines@gmail.com>
2015-02-23 13:36:23 -0500

    node & links

--- say.lisp
+++ say.lisp
@@ -1,10 +1,15 @@
-(load "/home/cyborg/Programs/noob-has-a-lisp/shelisp.lisp")
-(load "~/quicklisp/setup.lisp")
+(load "/home/cyborg/rouge_lisp/shelisp.lisp")
+(load "/home/cyborg/quicklisp/setup.lisp")
 (ql:quickload "drakma")
 
+
 (defun say (words)
+  (if (deactivate_voice) (print words)
   (script
-   (concatenate 'string "echo " words "| festival --tts")))
+   (concatenate 'string "echo " words "| festival --tts"))))
+
+(defun deactivate_voice ()
+    (equal (machine-instance) "evo-van"))
 
 (defun respond_dumb(words)
   (say
@@ -15,5 +20,18 @@
 					  drakma:*text-content-types*))
   (drakma:http-request "https://api.github.com/users/slightlycyborg"))
 
+(defun encourage_me ()
+  (say "You can do it")
+  )
+
+(defclass node () 
+  ((text
+	  :initarg :text)
+	(links
+	  :initform (make-array 1 :adjustable t :fill-pointer 0))))
+
+(defun link (from_node to_node)
+  (vector-push-extend to_node (slot-value from_node 'links)))
+  
 
 
